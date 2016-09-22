94906e9e9e15e40492d5750f8552224a695fb6df
andesitedesign@gmail.com <andesitedesign@gmail.com>
2016-09-22 11:03:17 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import App from './components/App';
+
+// Render
+render(
+  <App />,
+  document.getElementById('root')
+);
